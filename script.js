777cce90c68c74fa4e6cd4f818839c8e5872a6dc
jay-web <jay.developer12@gmail.com>
2021-03-07 16:26:21 +0530

    initial commit, mapbox integrated, marker integrated

---
+++ script.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoianNkZXZlbG9wZXIiLCJhIjoiY2tpemxnYnlxMmlrMzJ4c2N6cTQ0cnVxZyJ9.GSz0IUv_9wcuy5Yqwv8ldA";
+    const coords = [longitude, latitude];
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: coords, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+    });
+
+    function createMarker(coords, message) {
+      var marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+
+      var popup = new mapboxgl.Popup({
+        closeOnClick: false,
+        anchor: "bottom",
+        offset: 16
+        // className: 'running-popup'
+      })
+        .setLngLat(coords)
+        .setHTML(`<h3>${message}</h3>`)
+        .addTo(map);
+
+        // popup.removeClassName('mapboxgl-popup-content');
+        // popup.addClassName('running-popup');
+    }
+
+    createMarker(coords, 'you are here!');
+    map.on("click", function (e) {
+      form.classList.remove("hidden");
+      // let { lng, lat } = e.lngLat;
+      // createMarker([lng, lat], 'workout');
+      console.log(e.lngLat);
+    });
+  },
+  function () {
+    alert("Didn't work");
+  }
+);
