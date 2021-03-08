095ee049a43896a3bcc33fe595d9f3953279359a
jay-web <jay.developer12@gmail.com>
2021-03-08 19:49:52 +0530

    Changed application architecture Applied OOP Created App class to handle the application features and state Created _getPosition , _showForm, toggle fields, constructor functions in App class Attached the event listeners with constructor function.

--- script.js
+++ script.js
@@ -11,78 +11,97 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-let map, mapEvent;
-let lngLat;
+// let map, mapEvent;
+// let lngLat;
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
+// MAIN CLASS
+
+class App {
+  #map;
+  #mapEvent;
+  #lngLat;
+
+  constructor() {
+    this._getCurrentLocation();
+    form.addEventListener("submit", this._submitForm.bind(this));
+    inputType.addEventListener("change", this._toggleformFields);
+  }
+
+  // get current location
+  _getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert("Didn't get the location");
+      }
+    );
+  }
+
+  // ? Load Map after fetching location
+
+  _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
-    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+    // console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoianNkZXZlbG9wZXIiLCJhIjoiY2tpemxnYnlxMmlrMzJ4c2N6cTQ0cnVxZyJ9.GSz0IUv_9wcuy5Yqwv8ldA";
+    mapboxgl.accessToken = api.API_KEY; // integrate mapbox api key
+
     const coords = [longitude, latitude];
-    map = new mapboxgl.Map({
+    this.#map = new mapboxgl.Map({
       container: "map",
       style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
       center: coords, // starting position [lng, lat]
       zoom: 15, // starting zoom
     });
 
-
-    createMarker(coords, 'you are here!');
+    this._createMarker(coords, "you are here!");
 
     // Handling click on map event
-    map.on("click", function (e) {
-      mapEvent = e;
-      form.classList.remove("hidden");
-      inputDistance.focus();
-      lngLat = e.lngLat;        // mapbox using it in reverse lng first 
-      // let { lng, lat } = e.lngLat;
-      // createMarker([lng, lat], 'workout');
-      console.log(e.lngLat);
-    });
-  },
-  function () {
-    alert("Didn't work");
+    this.#map.on("click", this._showForm.bind(this));
   }
-);
 
-// ? Create marker function
-function createMarker(coords, message) {
-  var marker = new mapboxgl.Marker().setLngLat(coords).addTo(map);
+  // ? Create marker function
+  _createMarker(coords, message) {
+    var marker = new mapboxgl.Marker().setLngLat(coords).addTo(this.#map);
 
-  var popup = new mapboxgl.Popup({
-    closeOnClick: false,
-    anchor: "bottom",
-    offset: 16
-    // className: 'running-popup'
-  })
-    .setLngLat(coords)
-    .setHTML(`<h3>${message}</h3>`)
-    .addTo(map);
+    var popup = new mapboxgl.Popup({
+      closeOnClick: false,
+      anchor: "bottom",
+      offset: 16,
+      // className: 'running-popup'
+    })
+      .setLngLat(coords)
+      .setHTML(`<h3>${message}</h3>`)
+      .addTo(this.#map);
+  }
 
-    // popup.removeClassName('mapboxgl-popup-content');
-    // popup.addClassName('running-popup');
+  // ? show form on click on map
+
+  _showForm(e) {
+    this.#mapEvent = e;
+    form.classList.remove("hidden");
+    inputDistance.focus();
+    this.#lngLat = e.lngLat; // mapbox using it in reverse lng first
+    // let { lng, lat } = e.lngLat;
+    // createMarker([lng, lat], 'workout');
+    console.log(e.lngLat);
+  }
+
+  // ? handle form toggle fields
+  _toggleformFields() {
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  // ? Submit form / or create new workout
+  _submitForm(e) {
+    e.preventDefault();
+    let { lng, lat } = this.#lngLat;
+    this._createMarker([lng, lat], "workout");
+
+    inputDistance.value = inputDuration.value = "";
+    inputCadence.value = inputElevation.value = "";
+  }
 }
 
-
-// ? Handling form submit event
-
-form.addEventListener("submit", function(e){
-  e.preventDefault();
-  let {lng, lat } = lngLat;
-  createMarker([lng, lat], 'workout');
-
-  inputDistance.value = inputDuration.value = '';
-  inputCadence.value = inputElevation.value = '';
-
-})
-
-// ? Handling change event trigger on select form type input
-
-inputType.addEventListener("change", function(){
-  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-})
+const app = new App();
